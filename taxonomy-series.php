1b03bfcc1f2b3537131f8a573fd189363e6f0390
Irina <sokolovskaja.irina@gmail.com>
2014-05-28 10:13:27 +0600

    zip

--- taxonomy-series.php
+++ taxonomy-series.php
@@ -19,7 +19,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'api_url' => 'https://api.github.com/repos/ierhyna/taxonomy-series', // the github API url of your github repo
         'raw_url' => 'https://raw.github.com/ierhyna/taxonomy-series/master', // the github raw url of your github repo
         'github_url' => 'https://github.com/ierhyna/taxonomy-series', // the github url of your github repo
-        'zip_url' => 'https://github.com/ierhyna/taxonomy-series/zipball/master', // the zip url of the github repo
+        'zip_url' => 'https://github.com/ierhyna/taxonomy-series/archive/master.zip', // the zip url of the github repo
         'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.9.1', // which version of WordPress is your plugin tested up to?
