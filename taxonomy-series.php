7e8b6071a088663a0094b606ab3b51e3925fb1c2
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2015-01-11 13:36:47 +0300

    updater

--- taxonomy-series.php
+++ taxonomy-series.php
@@ -10,6 +10,30 @@ License: GPLv2
 */
 
 
+add_action( 'init', 'github_plugin_updater' );
+function github_plugin_updater() {
+	include_once 'updater.php';
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'taxonomy-series',
+			'api_url' => 'https://api.github.com/repos/ierhyna/taxonomy-series',
+			'raw_url' => 'https://raw.github.com/ierhyna/taxonomy-series/master',
+			'github_url' => 'https://github.com/ierhyna/taxonomy-series',
+			'zip_url' => 'https://github.com/ierhyna/taxonomy-series/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+		new WP_GitHub_Updater( $config );
+	}
+}
+
+
+
 // Register Custom Taxonomy
 function taxonomy_series() {
 
